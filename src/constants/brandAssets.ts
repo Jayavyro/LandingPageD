de6dad60409a55a7e9da59e3c4d14e6d0aa6f459
Jay <jaggarwal@avyro.in>
2026-07-17 13:14:59 +0530

    Add Dark Avyro logo for mobile navbar and update brand assets constants

--- src/constants/brandAssets.ts
+++ src/constants/brandAssets.ts
@@ -1,6 +1,7 @@
 import navbarLogoIcon from '../logoNew.jpg'
+import mobileNavbarLogo from '../Dark Avyro.png'
 import brandLogoExtended from '../WhiteAVyrobackgorund.png'
 
 export const NAVBAR_LOGO_ICON = navbarLogoIcon
-export const MOBILE_NAVBAR_LOGO = brandLogoExtended
+export const MOBILE_NAVBAR_LOGO = mobileNavbarLogo
 export const FOOTER_LOGO = brandLogoExtended
